082934a4d40de456c813eec7d1b679f042983bc2
luyuan <1324725249@qq.com>
2022-09-22 17:37:45 +0800

    common-parser init

---
+++ codec/codec.go
@@ -0,0 +1,36 @@
+package codec
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/simapp/params"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/cosmos/cosmos-sdk/x/auth/signing"
+	"github.com/tendermint/tendermint/types"
+)
+
+var (
+	AppModules []module.AppModuleBasic
+	Encodecfg  params.EncodingConfig
+)
+
+func GetTxDecoder() sdk.TxDecoder {
+	return Encodecfg.TxConfig.TxDecoder()
+}
+
+func GetMarshaler() codec.Codec {
+	return Encodecfg.Marshaler
+}
+
+func GetSigningTx(txBytes types.Tx) (signing.Tx, error) {
+	Tx, err := GetTxDecoder()(txBytes)
+	if err != nil {
+		return nil, err
+	}
+	signingTx := Tx.(signing.Tx)
+	return signingTx, nil
+}
+
+func RegisterAppModules(module ...module.AppModuleBasic) {
+	AppModules = append(AppModules, module...)
+}
